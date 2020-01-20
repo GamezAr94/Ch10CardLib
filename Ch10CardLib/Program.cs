c157ec5bd8274454477f176e032db151bd41bb4d
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-01-19 23:10:42 -0800

    adding loops to check the suit of 5 cards
    adding for loops to check 5 cards from a shuffled Deck. if all five cards are the same suit, then the client should display the card names on screen along with the text "Flush"; otherwise, it should quit after 50 cards with the text "No Flush."

--- Ch10CardLib/Program.cs
+++ Ch10CardLib/Program.cs
@@ -6,9 +6,10 @@ namespace Ch10CardLib
     {
         static void Main(string[] args)
         {
-            Deck myDeck = new Deck();
-            myDeck.Shuffle();
-            myDeck.displayDeck();
+            //Deck myDeck = new Deck();
+            //myDeck.Shuffle();
+            //myDeck.displayDeck();
+            bool isFlush = false;
 
             #region
             /*
@@ -23,6 +24,44 @@ namespace Ch10CardLib
             }
             */
             #endregion
+            while (!isFlush)
+            {
+                Deck myDeck = new Deck();
+                isFlush = false;
+                myDeck.Shuffle();
+                int handFlushIndex = 0;
+                for(int hand = 0; hand < 10; ++hand)
+                {
+                    isFlush = true;
+                    Suit suitCard = new Suit();
+                    suitCard = myDeck.getCard(hand * 5).suit;
+                    for(int card = 1; card < 5; ++card)
+                    {
+                        if (suitCard != myDeck.getCard(hand * 5 + card).suit)
+                        {
+                            isFlush = false;
+                        }
+                    }
+                    if (isFlush)
+                    {
+                        handFlushIndex = hand * 5;
+                        break;
+                    }
+                }
+                if (isFlush)
+                {
+                    Console.WriteLine("Flush!!!");
+                    for (int i = 0; i < 5; ++i)
+                    {
+                        Console.Write(myDeck.getCard(handFlushIndex + i) + " ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Flush.");
+                }
+                Console.ReadLine();
+            }
 
             Console.ReadKey();
         }

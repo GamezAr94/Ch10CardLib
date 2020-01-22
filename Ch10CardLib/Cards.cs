4d05b24a26dd1f795b19e7c2f9f8eb48bf12d112
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-01-21 21:06:14 -0800

    Adding the code
    adding the code in the Cards class.

--- Ch10CardLib/Cards.cs
+++ Ch10CardLib/Cards.cs
@@ -6,7 +6,31 @@ using System.Text;
 
 namespace Ch11CardLib
 {
-    public class Cards : CollectionBase
+    class Cards : CollectionBase
     {
+        public void Add(Cards newCard)
+        {
+            List.Add(newCard);
+        }
+        public void Remove(Cards newCard)
+        {
+            List.Remove(newCard);
+        }
+        public Card this[int cardIndex]
+        {
+            get { return (Card)List[cardIndex]; }
+            set { List[cardIndex] = value; }
+        }
+        public void CopyTo(Cards targetCards)
+        {
+            for(int index = 0; index < this.Count; index++)
+            {
+                targetCards[index] = this[index];
+            }
+        }
+        public bool Contains(Card card)
+        {
+            return InnerList.Contains(card);
+        }
     }
 }

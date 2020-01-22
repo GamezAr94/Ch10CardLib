d5ca40517376b66b80dab3e9add160741b913cf9
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-01-21 20:56:19 -0800

    Creating a nwe class
    this class will hold the code to the collection.

---
+++ Ch10CardLib/Cards.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch11CardLib
+{
+    public class Cards : CollectionBase
+    {
+    }
+}

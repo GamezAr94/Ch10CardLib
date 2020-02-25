d26e3c7a1c48ab4d4c74fb37f3f026d8850b92b2
GamezArt94 <46079765+GamezAr94@users.noreply.github.com>
2020-02-25 13:23:05 -0800

    Creating Cards Collection
    Creating a Cards class, it holds a collection of cards using System.Collection, CollectionBase

---
+++ Ch10CardLib/Cards.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch11CardLib
+{
+    class Cards : CollectionBase
+    {
+        public void Add(Card newCard)
+        {
+            List.Add(newCard);
+        }
+        public void Remove(Card oldCard)
+        {
+            List.Remove(oldCard);
+        }
+        public Card this[int cardIndex]
+        {
+            get => (Card)List[cardIndex];
+            set => List[cardIndex] = value;
+        }
+        public void CopyTo(Cards targetCards)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                targetCards[i] = this[i];
+            }
+        }
+        public bool Contains(Card card) => InnerList.Contains(card);
+    }
+}
